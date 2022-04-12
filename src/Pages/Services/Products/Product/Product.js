d8f1f0ef81f82e408f05ec90b8dec2a02d7b0d33
MK-Raihan <mkraihan207@gmail.com>
2022-04-12 23:47:23 +0600

    products

--- src/Pages/Services/Products/Product/Product.js
+++ src/Pages/Services/Products/Product/Product.js
@@ -6,24 +6,22 @@ const Product = (props) => {
     const{_id, name, img, description, price}=props.product;
     return (
         <div className="col text-center">
-    <div className="card h-100 product-card">
-      <img src={img} className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title fs-4 fw-bold">{name}</h5>
-        <p className="card-text">{description}</p>
+    <div className=" h-100 product-card">
+      <img src={img} className="img-fluid" alt="..."/>
+      <div className="product-body">
+        <h5 className="product-title fs-4 fw-bold">{name}</h5>
+        <p className="product-text">{description}</p>
         <p className='price '>{price}</p>
       </div>
-      <div className="card-body ">
+      <div className="product-body ">
             <NavLink to={`/booking/${_id}`}>
-            <button className="btn me-2 order-btn">ORDER NOW</button>
+            <button className="btn me-4 order-btn fs-5"><i className="fas fa-cart-arrow-down"></i></button>
             </NavLink>
-            <NavLink to={`/details/${_id}`}>
-            <button className="btn  order-btn">Details</button>
+            <NavLink to={`/booking/${_id}`}>
+            <button className="btn  order-btn fs-5"><i class="fas fa-solid fa-arrow-right"></i></button>
             </NavLink>
       </div>
-      <div className="card-footer d-flex justify-content-evenly">
-        raihan
-      </div>
+      
     </div>
   </div>
     );
